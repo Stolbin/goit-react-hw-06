0f3483c6259d6a288c6132c6a6dd7ab1185029e3
Stolbin <109080579+Stolbin@users.noreply.github.com>
2024-04-25 13:45:49 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import storage from "redux-persist/lib/storage";
 import contactsSlice from "./contactsSlice";
-import { filtersReducer } from "./filters";
+import { filtersReducer } from "./filtersSlice";
 
 import {
   persistStore,
